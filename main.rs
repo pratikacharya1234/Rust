d1f653d80a87fb2510808504837964da078afbbc
pratikacharya1234 <acharyapratik214@gmail.com>
2025-09-19 15:23:02 +0100

    Refactor main function to include city input and improve output formatting

--- main.rs
+++ main.rs
@@ -1,3 +1,7 @@
+//standard library --helps to take user input
+use std::io;
+
+
 fn main(){
 
     //Print hello world
@@ -7,6 +11,15 @@ fn main(){
     let  first_name = "Pratik";
     let  last_name = "Acharya";
 
-    println!("{} {}", first_name, last_name);
+    
+    
+    // assign variables using mut
+    println!("Enter Your City: ");
+    let mut city_name = String::new();
+    
+    io::stdin()
+    .read_line(&mut city_name)
+    .expect("Failed to read line");
 
+    println!("{} {} {}", first_name, last_name, city_name);
 }
